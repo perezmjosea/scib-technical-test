68744317e281df19f1c0184c8a6ea530c617e04f
perezmjosea <perezm.josea@gmail.com>
2025-05-06 14:59:10 +0200

    BACK - Services and Controller

--- scib-back/src/controllers/person/person.controller.ts
+++ scib-back/src/controllers/person/person.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PersonService } from '../../services/person/person.service';
+import { PersonDTO } from '../../services/dto/person.dto';
 
 @Controller('person')
-export class PersonController {}
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  @Post()
+  savePerson(person: PersonDTO): PersonDTO {
+    return this.personService.savePerson(person);
+  }
+
+  @Get()
+  getAllPersons(): PersonDTO[] {
+    return this.personService.getAllPersons();
+  }
+}
--- scib-back/src/services/person/person.service.ts
+++ scib-back/src/services/person/person.service.ts
@@ -1,10 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PersonDTO } from '../dto/person.dto';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
-export class PersonService {
+export class PersonService implements OnModuleInit {
+  private readonly _filePath = path.resolve(__dirname, '../db/persons.json');
   private _persons: PersonDTO[] = [];
+
+  onModuleInit() {
+    this._readJSON();
+  }
+
+  private _readJSON() {
+    if (!fs.existsSync(this._filePath)) {
+      fs.writeFileSync(this._filePath, '[]');
+    }
+    if (fs.existsSync(this._filePath)) {
+      try {
+        const content = fs.readFileSync(this._filePath, 'utf-8');
+        this._persons = JSON.parse(content || '[]') as PersonDTO[];
+      } catch (error) {
+        console.error('Error on JSON file:', error);
+        this._persons = [];
+      }
+    }
+  }
   savePerson(person: PersonDTO) {
     this._persons.push(person);
     return person;
