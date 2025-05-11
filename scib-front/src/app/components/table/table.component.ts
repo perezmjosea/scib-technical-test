d993eb19329abdc6edda75bcbb954260d23c7399
perezmjosea <perezm.josea@gmail.com>
2025-05-12 00:59:28 +0200

    Add pagination

--- scib-front/src/app/components/table/table.component.ts
+++ scib-front/src/app/components/table/table.component.ts
@@ -1,28 +1,39 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { AfterViewInit, Component, computed, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonService } from '../../services/person/person.service';
 import { Person } from '../../interfaces/person';
-import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatCardModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatTableModule, MatCardModule, MatProgressSpinnerModule, MatPaginatorModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   private _personService = inject(PersonService);
+
   loading = signal(true);
   persons = computed(() => this._personService.persons());
+  dataSource = new MatTableDataSource<Person>();
   displayedColumns: (keyof Person)[] = ['name', 'surname', 'seniority', 'yearsOfExperience', 'availability'];
 
   ngOnInit(): void {
     this._personService.getAllPersons().subscribe((data) => {
       this._personService.persons.set(data);
-      setTimeout(() => { this.loading.set(false); }, 1000);
+      this.dataSource.data = data;
     });
+    setTimeout(() => {
+      this.loading.set(false);
+    }, 1000);
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 }
