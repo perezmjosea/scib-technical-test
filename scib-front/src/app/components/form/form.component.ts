a5fbeb51eca37b7b591bfc870c67cf3fa4e4e6f6
perezmjosea <perezm.josea@gmail.com>
2025-05-11 23:22:47 +0200

    Improve styles and code

--- scib-front/src/app/components/form/form.component.ts
+++ scib-front/src/app/components/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonService } from '../../services/person/person.service';
-import { Person } from '../../interfaces/person';
 import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -24,24 +23,23 @@ export class FormComponent {
   private _snackBar = inject(MatSnackBar);
 
   file: File | null = null;
+  invalidFile: boolean = false;
   fileTouched = false;
   fileData: (string | number | boolean)[] = [];
-  sendingData: boolean = false
+  sendingData = false;
+
   form = this._formBuilder.group({
     name: ['', Validators.required],
     surname: ['', Validators.required],
     file: [null, Validators.required]
-  })
+  });
 
   private _isValidFileData(data: unknown): data is [string, number, boolean] {
-    if (!Array.isArray(data) || data.length !== 3) {
-      return false
-    };
+    if (!Array.isArray(data) || data.length !== 3) return false;
     const [seniority, years, availability] = data;
     const validSeniority = seniority === 'Junior' || seniority === 'Senior';
     const validYears = typeof years === 'number' && !isNaN(years);
     const validAvailability = typeof availability === 'boolean' || availability === 'Yes' || availability === 'No';
-
     return validSeniority && validYears && validAvailability;
   }
 
@@ -49,6 +47,7 @@ export class FormComponent {
     const input = event.target as HTMLInputElement;
     const selectedFile = input.files?.[0];
     this.fileTouched = true;
+    console.log('File selected:', selectedFile);
 
     if (!selectedFile) {
       this.file = null;
@@ -61,18 +60,21 @@ export class FormComponent {
     fileReader.onload = (e) => {
       const data = new Uint8Array(e.target?.result as ArrayBuffer);
       const book = XLSX.read(data, { type: 'array' });
-      const sheetName = book.SheetNames[0];
-      const sheet = book.Sheets[sheetName];
+      const sheet = book.Sheets[book.SheetNames[0]];
       const rawData = XLSX.utils.sheet_to_json(sheet, { header: 1 })[1];
-      console.log(rawData);
 
       if (!this._isValidFileData(rawData)) {
         this.fileData = [];
-        this._snackBar.open('Archivo inválido. Debe contener: seniority ("junior" o "senior"), años (número) y disponibilidad (booleano).', 'Cerrar', {
-          duration: 5000
-        });
+        this.invalidFile = true;
+        this.file = null;
+        this._snackBar.open(
+          'Archivo inválido. Debe contener: seniority ("junior" o "senior"), años (número) y disponibilidad (booleano).',
+          'Cerrar',
+          { duration: 5000 }
+        );
         return;
       }
+
       this.fileData = rawData;
     };
 
@@ -80,10 +82,9 @@ export class FormComponent {
   }
 
   onFormSubmit() {
-    this.form.markAllAsTouched();
-    this.fileTouched = true;
-
     if (!this.file || this.form.invalid) {
+      this.form.markAllAsTouched();
+      this.fileTouched = true;
       return;
     }
 
@@ -91,8 +92,8 @@ export class FormComponent {
     this.form.disable();
 
     const formData = new FormData();
-    formData.append('name', this.form.value.name as string);
-    formData.append('surname', this.form.value.surname as string);
+    formData.append('name', this.form.value.name!);
+    formData.append('surname', this.form.value.surname!);
     formData.append('file', this.file);
 
     this._personService.postNewPerson(formData).subscribe({
@@ -111,8 +112,16 @@ export class FormComponent {
   resetForm() {
     this.sendingData = false;
     this.file = null;
+    this.fileTouched = false;
+    this.fileData = [];
     this.form.enable();
     this.form.reset();
-    this.form.markAsUntouched();
+    Object.keys(this.form.controls).forEach((key) => {
+      const control = this.form.get(key);
+      control?.setErrors(null);
+      control?.markAsPristine();
+      control?.markAsUntouched();
+    });
+    this.form.updateValueAndValidity();
   }
 }
